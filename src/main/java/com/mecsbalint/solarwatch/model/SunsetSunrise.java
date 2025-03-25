1e1361a2b62729950f8ae6cc1f5b3a0d6d584931
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-03-25 15:47:55 +0100

    Set up Lombok

--- src/main/java/com/mecsbalint/solarwatch/model/SunsetSunrise.java
+++ src/main/java/com/mecsbalint/solarwatch/model/SunsetSunrise.java
@@ -1,10 +1,12 @@
 package com.mecsbalint.solarwatch.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class SunsetSunrise {
 
     @Id
@@ -16,36 +18,4 @@ public class SunsetSunrise {
     private LocalDate date;
     private String sunrise;
     private String sunset;
-
-    public City getCity() {
-        return city;
-    }
-
-    public String getSunrise() {
-        return sunrise;
-    }
-
-    public String getSunset() {
-        return sunset;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setCity(City city) {
-        this.city = city;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public void setSunrise(String sunrise) {
-        this.sunrise = sunrise;
-    }
-
-    public void setSunset(String sunset) {
-        this.sunset = sunset;
-    }
 }
