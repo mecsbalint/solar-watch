07df3408a7f2d8dbef091f6a4d31fbb9d80d2f85
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-03-26 17:50:22 +0100

    Implement Security configuration

--- src/main/java/com/mecsbalint/solarwatch/repository/UserRepository.java
+++ src/main/java/com/mecsbalint/solarwatch/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.mecsbalint.solarwatch.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+    Optional<UserEntity> findByName(String name);
 }
